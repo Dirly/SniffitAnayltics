3e56421cc0eb7c4fadc8f5193aa9a98f21823acb
Dirly <derek.kaleida@gmail.com>
2015-08-25 16:06:31 -0400

    trying to test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var server = require('http').createServer(),
 	url = require('url'), 
 	WebSocketServer = require('ws').Server,
-		wss = new WebSocketServer({ server: server }),
-		express = require('express'), 
-		app = express(), 
-		port = 4080;
+	wss = new WebSocketServer({ server: server }),
+	express = require('express'), 
+	app = express(), 
+	port = 4080;
 
 app.use(function (req, res) {
   res.send({ msg: "hello" });
@@ -14,8 +14,6 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
-
-  ws.send('something');
 });
 
 server.on('request', app);
