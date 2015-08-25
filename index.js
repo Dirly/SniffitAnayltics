0166f8a14682bb8406a068c3a5073fdab34c6ff7
Dirly <derek.kaleida@gmail.com>
2015-08-25 15:13:55 -0400

    herku test

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var webSocketServer = require('ws').Server,
+/*var server = require('http').createServer(), 
+	WebSocketServer = require('ws').Server,
+	wss = new WebSocketServer({ server: server }),
+	express = require('express'),
+	app = express(),
+	port = 4080;
 
-wss = new WebSocketServer ({port: 3001, path: '/path'});
-wss.on('listening', function() {
-    console.info('ws server is listening');
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+
+	ws.send('something');
+});
+
+server.on('request', app);
+server.listen(port, function () {
+
+});*/
+
+
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
 });
