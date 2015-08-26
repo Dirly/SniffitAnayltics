c2dbbc83fcd4264cf7953ab0535040007bdb3155
Dirly <derek.kaleida@gmail.com>
2015-08-25 21:06:45 -0400

    moving heroku

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var WebSocketServer = require("ws").Server,
 	http = require("http"),
 	express = require("express"),
+	mongoose = require("mongoose"),
 	app = express(),
 	port = process.env.PORT || 5000;
+
+
+var uristring =
+	process.env.MONGOLAB_URI ||
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/HelloMongoose';
 
 app.use(express.static(__dirname + "/"));
 
@@ -17,11 +24,20 @@ console.log("websocket server created");
 wss.on("connection", function(ws) {
 	console.log("websocket connection open");
 
+	mongoose.connect(uristring, function (err, res) {
+		if (err) {
+			console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+		} else {
+			console.log ('Succeeded connected to: ' + uristring);
+		}
+	});
+
 	ws.on('message', function incoming(message) {
-		console.log('received: %s', message);
+		console.log('received:', message);
 	});
 
 	ws.on("close", function() {
 		console.log("websocket connection close");
 	});
 });
+
