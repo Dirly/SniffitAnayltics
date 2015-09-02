4650e101b020abd982c557779dcbf44a9346c283
dkaleida <dkaleida@red-nucleus.com>
2015-09-02 13:10:38 -0400

    changed to sommething else

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ wss.on("connection", function(ws) {
 					});
 				} else if (data.command ==="sumTotal"){
 					console.log("got here");
-					var sumTotal = uristring.ScriptSniffed.aggregate([
+					var sumTotal = uristring.SSniffed.aggregate([
 						{
 							$group : {
 								_id : null,
