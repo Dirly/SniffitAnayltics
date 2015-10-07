6dee88f82ec06a7b4019e5701d0da052e7ad9fc3
dkaleida <dkaleida@red-nucleus.com>
2015-10-07 17:25:59 -0400

    update test

--- index.js
+++ index.js
@@ -35,48 +35,49 @@ wss.on("connection", function(ws) {
 	console.log("websocket connection open");
 	ws.on('message', function incoming(message) {
 		mongoose.connect(uristring, function (err, db) {
-			if (err) {
-				console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-			} else {
-				console.log('connection to DB established');
-				var data = JSON.parse(message);
+			ws2.on('open', function open() {
 
-				if(data.command === "post"){
-					SSniffed.findById(data.id, function(err,target){
-						if(target){
-							console.log("entry found");
-							target.hours = data.hours;
-							target.lines = data.lines;
-							target.sniffedEvents = data.sniffedEvents; 
-							target.totalEvents = data.totalEvents;
-							target.save(function (err) {
-								if (err){
-									 console.log ('Error on save!', err);
-								} else {
-									ws.close();
-								}
-							});
-							ws.send("updated");
-						} else {
-							console.log("entry NOT found");
-							var newData = new SSniffed({
-								hours: data.hours,
-								lines: data.lines,
-								sniffedEvents: data.sniffedEvents,
-								totalEvents: data.totalEvents
-							});
-							newData.save(function (err) {
-								if (err){
-									 console.log ('Error on save!', err);
-								} else {
-									ws.send(newData.id);
-									ws.close();
-								}
-							});
-						}
-					});
+				if (err) {
+					console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+				} else {
+					console.log('connection to DB established');
+					var data = JSON.parse(message);
+
+					if(data.command === "post"){
+						SSniffed.findById(data.id, function(err,target){
+							if(target){
+								console.log("entry found");
+								target.hours = data.hours;
+								target.lines = data.lines;
+								target.sniffedEvents = data.sniffedEvents; 
+								target.totalEvents = data.totalEvents;
+								target.save(function (err) {
+									if (err){
+										 console.log ('Error on save!', err);
+									} else {
+										ws.close();
+									}
+								});
+								ws.send("updated");
+							} else {
+								console.log("entry NOT found");
+								var newData = new SSniffed({
+									hours: data.hours,
+									lines: data.lines,
+									sniffedEvents: data.sniffedEvents,
+									totalEvents: data.totalEvents
+								});
+								newData.save(function (err) {
+									if (err){
+										 console.log ('Error on save!', err);
+									} else {
+										ws.send(newData.id);
+										ws.close();
+									}
+								});
+							}
+						});
 				/*} else if (data.command ==="sumTotal"){*/
-					ws2.on('open', function open() {
 						SSniffed.aggregate(
 							[
 								{
@@ -97,30 +98,32 @@ wss.on("connection", function(ws) {
 								ws2.close();
 							}
 						);
-					});
-				} else if (data.command ==="sumTotal"){
-					SSniffed.aggregate(
-						[
-							{
-								$group : {
-									_id : null,
-									totalHours: {$sum: "$hours" },
-									totalLines: {$sum: "$lines" },
-									totalSniffed: {$sum: "$sniffedEvents" }
+					
+					} else if (data.command ==="sumTotal"){
+						SSniffed.aggregate(
+							[
+								{
+									$group : {
+										_id : null,
+										totalHours: {$sum: "$hours" },
+										totalLines: {$sum: "$lines" },
+										totalSniffed: {$sum: "$sniffedEvents" }
+									}
 								}
+							], function (err, result) {
+								if (err) {
+									console.log(err);
+									return;
+								}
+								console.log(result);
+								ws.send(JSON.stringify(result));
+								ws.close();
 							}
-						], function (err, result) {
-							if (err) {
-								console.log(err);
-								return;
-							}
-							console.log(result);
-							ws.send(JSON.stringify(result));
-							ws.close();
-						}
-					);
+						);
+					}
+
 				}
-			}
+			});
 		});
 	});
 
